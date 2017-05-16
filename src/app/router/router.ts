9c37addf20a0f53b42dfc551e21b0f42e3006b53
Thierry Belliot <thierrybelliot@gmail.com>
2017-05-16 15:03:58 +0200

    add overview

--- src/app/router/router.ts
+++ src/app/router/router.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from '../login/login/login.component';
+import { OverviewComponent } from '../overview/overview/overview.component';
 
 export const router: Routes = [
     {
