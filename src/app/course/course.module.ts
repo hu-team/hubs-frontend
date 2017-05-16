2c4d0d7912ad57a16e18613957ea83a5efd88873
Thierry Belliot <thierrybelliot@gmail.com>
2017-05-16 15:49:00 +0200

    course over view

---
+++ src/app/course/course.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CourseOverviewComponent } from './course-overview/course-overview.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CourseOverviewComponent]
+})
+export class CourseModule { }
